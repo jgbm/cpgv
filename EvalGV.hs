33a85b44d42bc08ef6f06194144c9a22b96e2d3e
sam <Sam.Lindley@ed.ac.uk>
2013-10-18 15:44:41 +0100

    a big-step semantics for GV

--- EvalGV.hs
+++ EvalGV.hs
@@ -5,73 +5,55 @@ import Control.Monad
 import Data.Maybe
 
 data Value =
-   VVar LIdent
- | VUnit
- | VFun LIdent Term
+   VUnit
+ | VFun Var Term
  | VPair Value Value
- | VChannel CIdent
+ | VLabel Label
+ | VChannel Chan
    deriving (Eq,Ord,Show)
 
-valueToTerm :: Value -> Term
-valueToTerm (VVar x)     = Var x
-valueToTerm VUnit        = Unit
-valueToTerm (VFun x e)   = Fun x e
-valueToTerm (VPair v w)  = Pair (valueToTerm v) (valueToTerm w)
-valueToTerm (VChannel c) = undefined
+-- valueToTerm :: Value -> Term
+-- valueToTerm VUnit        = Unit
+-- valueToTerm (VFun x e)   = Fun x e
+-- valueToTerm (VPair v w)  = Pair (valueToTerm v) (valueToTerm w)
+-- valueToTerm (VChannel c) = undefined
 
--- shouldn't need this
--- data EvalContext =
---    Hole
---  | AppFun EvalContext Term
---  | AppArg Value EvalContext
---  | PairLeft EvalContext Term
---  | PairRight Value EvalContext
---  | LetElim Pattern EvalContext Term
---  | SelectElim LIdent EvalContext
---  | CaseElim EvalContext [Branch]
---    deriving (Eq,Ord,Show)
+-- runtime channels
+type Port = Int
+type Chan = (Port, Port)
 
+type Var = LIdent
+type Label = LIdent
 
--- runtime channel identifier
-type CIdent = Int
-
-type Channel = (CIdent, [Value])
-type Gen = Int
+type Buffer = (Port, [Value])
 type Env k v = [(k, v)]
-type VEnv = Env LIdent Value
+type VEnv = Env Var Value
 
-type Configuration = (VEnv, [Channel], [Term], Gen)
-
-{-
-  TODO:
-    * refactor split
-    * name generation for with
-    * code for handling interactions between threads
-    * ...
--}
+type Configuration = ([Buffer], [Susp Value], Port)
 
 -- a possibly suspended value
 data Susp a =
-   Result a
- | Split VEnv Term Term
- | SSend Value CIdent (Value -> Susp a)
- | SReceive CIdent (Value -> Susp a)
- | SSelect LIdent CIdent (Value -> Susp a)
- | SCase CIdent [(LIdent, Value)] (Value -> Susp a)
+   Return a
+ | SWith (Chan -> (Susp Value, Susp Value)) (Value -> Susp a)
+ | SSend Value Chan (Value -> Susp a)
+ | SReceive Chan (Value -> Susp a)
+ | SSelect Label Chan (Value -> Susp a)
+ | SCase Chan (Env Label (Value -> Susp Value)) (Value -> Susp a)
 
 instance Monad Susp where
-  return          = Result
-  
-  --Wait e k' >>= k = Wait e (\v -> k' v >>= k)
+  return = Return
+  Return v       >>= k = k v  
+  SWith f k'     >>= k = SWith f     (k' >=> k)
+  SSend v c k'   >>= k = SSend v c   (k' >=> k)
+  SReceive c k'  >>= k = SReceive c  (k' >=> k)
+  SSelect l c k' >>= k = SSelect l c (k' >=> k)
+  SCase c bs k'  >>= k = SCase c bs  (k' >=> k)
 
-split = Split
-ssend v c = SSend v c return
-sreceive c = SReceive c return
+swith f     = SWith f return
+ssend v c   = SSend v c return
+sreceive c  = SReceive c return
 sselect l c = SSelect l c return
-scase v bs = SCase v bs return
-
--- suspend :: Term -> Susp Value
--- suspend e = Wait e return
+scase v bs  = SCase v bs return
 
 extend = flip (:)
 
@@ -94,7 +76,8 @@ evalThread env e = evalThread' env e where
     do v <- et e1
        evalThread' (extend env (x, v)) e2
   evalThread' env (With x e1 e2) =
-    split (extend env (x, undefined)) e1 e2
+    swith (\(c1, c2) -> (evalThread' (extend env (x, VChannel (c1, c2))) e1,
+                         evalThread' (extend env (x, VChannel (c2, c1))) e2))
   evalThread' env (End e)        = et e
   evalThread' env (Send m n)     =
     do v <- et m
@@ -108,14 +91,60 @@ evalThread env e = evalThread' env e where
        sselect l c
   evalThread' env (Case e bs) =
     do (VChannel c) <- et e
-       let bs' = map (\(Branch l x e) -> (l, VFun x e)) bs
+       let bs' = map (\(Branch l x e) -> (l, \v -> evalThread' (extend env (x, v)) e)) bs
        scase c bs'
 
+-- perform up to one concurrency step
+step :: Susp Value -> [Buffer] -> [Susp Value] -> Port -> Maybe Configuration
+step (Return _) _ _ _ = Nothing
+step (SWith f k) cs ts next = Just ((next+1, []):(next, []):cs, ts ++ [t1, t2 >>= k], next+2)
+  where
+    (t1, t2) = f (next, next+1)
+step (SSend v c k) cs ts next = Just (sendValue v c cs, ts ++ [k (VChannel c)], next)
+step (SReceive c k) cs ts next =
+  do (v, cs') <- receiveValue c cs 
+     return (cs', ts ++ [k (VPair v (VChannel c))], next)  
+step (SSelect l c k) cs ts next = Just (sendLabel l c cs, ts ++ [k (VChannel c)], next)
+step (SCase c bs k) cs ts next =
+  do (s, cs') <- receiveLabel c bs cs 
+     return (cs', ts ++ [s >>= k], next)
 
+sendValue :: Value -> Chan -> [Buffer] -> [Buffer]
+sendValue v (p, _) = map (\(q, vs) -> if p == q then (q, vs ++ [v])
+                                      else (q, vs))
 
+receiveValue :: Chan -> [Buffer] -> Maybe (Value, [Buffer])
+receiveValue (_, p) ((q, []):cs) | p == q = Nothing
+receiveValue (_, p) ((q, v:vs):cs) | p == q = Just (v, (q, vs):cs)
+receiveValue c      (c':cs) =
+  do (v, cs') <- receiveValue c cs
+     return (v, c':cs')
 
-eval :: Configuration -> Value
-eval = undefined
+sendLabel :: Label -> Chan -> [Buffer] -> [Buffer]
+sendLabel l (p, _) = map (\(q, vs) -> if p == q then (q, vs ++ [VLabel l])
+                                        else (q, vs))
 
-evalProgram :: Term -> Value
-evalProgram e = eval ([], [], [e], 0)
+receiveLabel :: Chan -> Env Label (Value -> Susp Value) -> [Buffer] -> Maybe (Susp Value, [Buffer])
+receiveLabel c@(_, p) bs ((q, []):cs) | p == q = Nothing
+receiveLabel c@(_, p) bs ((q, (VLabel l):vs):cs) | p == q = Just (v, (q, vs):cs)
+  where
+    v = matchLabel c l bs
+    matchLabel c l bs = f (VChannel c)
+      where f = fromJust (lookup l bs)
+receiveLabel c        bs (c':cs) =
+  do (t, cs') <- receiveLabel c bs cs
+     return (t, c':cs')
+
+evalConfig :: Configuration -> [Susp Value]
+evalConfig conf = evalConfig' 0 conf
+  where
+    -- keep going until all threads are blocked
+    evalConfig' :: Int -> Configuration -> [Susp Value]
+    evalConfig' n (cs, ts, next) | n >= length ts = ts
+    evalConfig' n (cs, t:ts, next) =
+      case step t cs ts next of
+        Nothing -> evalConfig' (n+1) (cs, ts ++ [t], next)
+        Just conf -> evalConfig conf
+
+evalProgram :: Term -> Susp Value
+evalProgram e = last (evalConfig ([], [evalThread [] e], 0))
